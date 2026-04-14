b0bebe211c847257fffd51a5a70a25c1bbf15b1f
Lee Qi Zheng <leeqz2001@gmail.com>
2026-04-14 13:41:19 +0800

    refactor(219-modify-phone-number): Implement more test cases

--- src/test/java/seedu/address/model/resident/PhoneTest.java
+++ src/test/java/seedu/address/model/resident/PhoneTest.java
@@ -31,6 +31,10 @@ public class PhoneTest {
         assertFalse(Phone.isValidPhone("91 ")); // less than 3 numbers with space
         assertFalse(Phone.isValidPhone(" 91")); // less than 3 numbers with space
         assertFalse(Phone.isValidPhone(" 91 ")); // less than 3 numbers with space
+        assertFalse(Phone.isValidPhone("123456789012345678901")); // 21 digits
+        assertFalse(Phone.isValidPhone("123456789012345678901 ")); // 21 digits with space
+        assertFalse(Phone.isValidPhone(" 123456789012345678901 ")); // 21 digits with space
+        assertFalse(Phone.isValidPhone("   123456789012345678901  ")); // 21 digits with space
         assertFalse(Phone.isValidPhone("phone")); // non-numeric
         assertFalse(Phone.isValidPhone("9011p041")); // alphabets within digits
         assertFalse(Phone.isValidPhone("9312 1534")); // spaces within digits
